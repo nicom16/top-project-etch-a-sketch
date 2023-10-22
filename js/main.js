8980696c48255d6709ba7c2e96b1cb69c4f3922f
Nicolò <nico.md.1600@gmail.com>
2023-10-22 11:13:35 +0200

    Added hover effect to change square background

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@
 // Grid 
 let gridContainer = document.querySelector('.grid-container');
 
-for (let i = 0; i < 16*16; i++) {
-  const square = document.createElement('div');
+for (let i = 0; i < 16 * 16; i++) {
+  let square = document.createElement('div');
   square.classList.add('square');
-  gridContainer.appendChild(square.cloneNode(true));
+
+  // Cloning square in order to be able to reiterate with for {}: without clonation it would stop at second iteration (.appendChild() removes the contenttent of the appended node)
+  let clonedSquare = square;
+
+  clonedSquare.addEventListener('mouseover', (event) => {
+    event.currentTarget.style.background = "black";
+  });
+
+  gridContainer.appendChild(clonedSquare);
 }
